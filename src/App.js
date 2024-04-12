98e331d5dbc2c01313a50a48813138ad9e3cf317
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-04-12 09:10:04 +0530

    Navigation set up for various paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./ components/Header";
+
 function App() {
-  return <h1 className="text-3xl text-orange-500 font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/add" element={<Header />} />
+        <Route path="/active" element={<Header />} />
+        <Route path="/completed" element={<Header />} />
+        <Route path="/notes" element={<Header />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
