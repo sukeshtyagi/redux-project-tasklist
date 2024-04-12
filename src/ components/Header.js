ec07b21f53a4b6bba5c8fdf6879f6a3117d4bbf2
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-04-12 09:39:51 +0530

    Header comp ui completed with items and navigation

--- src/ components/Header.js
+++ src/ components/Header.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
   return (
-    <div>Header</div>
-  )
+    <div className="header box-border w-fit m-auto flex justify-center items-center gap-12 p-5 bg-yellow-500">
+      <p
+        className="headerItem box-border hover:text-zinc-100 cursor-pointer"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        All tasks
+      </p>
+      <p
+        className="headerItem box-border hover:text-zinc-100 cursor-pointer"
+        onClick={() => {
+          navigate("/active");
+        }}
+      >
+        Active tasks
+      </p>
+      <p
+        className="headerItem box-border hover:text-zinc-100 cursor-pointer"
+        onClick={() => {
+          navigate("/completed");
+        }}
+      >
+        Completed tasks
+      </p>
+      <p
+        className="headerItem box-border hover:text-zinc-100 cursor-pointer"
+        onClick={() => {
+          navigate("/add");
+        }}
+      >
+        Add new task
+      </p>
+      <p
+        className="headerItem box-border hover:text-zinc-100 cursor-pointer"
+        onClick={() => {
+          navigate("/notes");
+        }}
+      >
+        Notes
+      </p>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
